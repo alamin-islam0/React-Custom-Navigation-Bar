247dadebc99a8ad72c2d07ba760e02d1cdb37e16
Alamin Islam <alamin648890@gmail.com>
2025-10-04 16:31:50 +0600

    navigation item added + dynamic navigation route and path added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ResponsiveNav from './components/Responsive Nav/ResponsiveNav'
+
+function App() {
+
+  return (
+    <>
+      <ResponsiveNav>
+        
+      </ResponsiveNav>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Responsive Nav/ResponsiveNav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const navLinks = [
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "About", path: "/about" },
+  { id: 3, name: "Services", path: "/services" },
+  { id: 4, name: "Blog", path: "/blog" },
+  { id: 5, name: "Contact", path: "/contact" }
+];
+
+const ResponsiveNav = () => {
+    return (
+        <ul className='flex'>
+            {
+                navLinks.map(route => <li className='mr-10 '>
+                    <a href={route.path}>{route.name}</a>
+                    </li>)
+            }
+        </ul>
+    );
+};
+
+export default ResponsiveNav;

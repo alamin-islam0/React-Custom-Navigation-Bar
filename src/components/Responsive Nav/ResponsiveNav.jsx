ecc5da548afeacc7bf3d77bcb1dec9e3b3889464
Alamin Islam <alamin648890@gmail.com>
2025-10-04 17:40:03 +0600

    solved bugs

--- src/components/Responsive Nav/ResponsiveNav.jsx
+++ src/components/Responsive Nav/ResponsiveNav.jsx
@@ -10,41 +10,45 @@ const navLinks = [
 ];
 
 const ResponsiveNav = () => {
+  const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false)
-
-    const links = navLinks.map((route) => (
-          <li className="mr-10 max-md:pt-4">
-            <a className="text-white" href={route.path}>
-              {route.name}
-            </a>
-          </li>
-        ))
+  const links = navLinks.map((route) => (
+    <li className="mr-10 max-md:mr-0 max-md:pt-4 max-md:pb-2">
+      <a
+        className="text-white hover:text-gray-200 transition-colors"
+        href={route.path}
+      >
+        {route.name}
+      </a>
+    </li>
+  ));
 
   return (
-    <nav className="flex justify-between px-16 py-6 bg-green-600 items-center max-md:px-2.5 max-md:py-4">
-      <span onClick={()=> setOpen(!open)} className="flex gap-2 items-center flex-row-reverse justify-between">
-        {
-            open ? 
-            <X className="size-12 md:hidden"></X> 
-            : <Menu color="white" className="md:hidden size-12 "></Menu>
-        }
+    <nav className="flex justify-between px-16 py-6 bg-green-600 items-center max-md:px-2.5 max-md:py-4 relative">
+      <div className="flex gap-2 items-center flex-row-reverse justify-between">
+        <button onClick={() => setOpen(!open)} className="md:hidden">
+          {open ? (
+            <X className="size-12"></X>
+          ) : (
+            <Menu color="white" className="size-12"></Menu>
+          )}
+        </button>
 
-        <ul className={`md:hidden absolute bg-green-500 duration-500 ${open ? 'top-24' : 'top-[-700px]'} w-[100%] px-10 py-8`}>
-            {links}
-        </ul>
-        
         <h3 className="text-2xl font-semibold text-white max-md:w-[50%]">
           React-Custom-Navigation-Bar
         </h3>
-      </span>
+      </div>
 
-      <ul className="md:flex hidden">
-        {
-            links
-        }
+      <ul
+        className={`md:hidden absolute bg-green-500 duration-500 ${
+          open ? "top-24" : "top-[-700px]"
+        } left-0 right-0 w-full py-8 px-4`}
+      >
+        {links}
       </ul>
-      <button 
+
+      <ul className="md:flex hidden">{links}</ul>
+      <button
         className=" max-md:hidden text-green-600 text-[18px] font-semibold bg-white px-8 py-3 rounded-md"
         type="button"
       >
